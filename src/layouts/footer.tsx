a7d81ae3365c4d4808361ea4eeed01dc79341399
abiasa <rnabiasa@gmail.com>
2026-01-08 09:46:50 +0700

    Add social link on footer

--- src/layouts/footer.tsx
+++ src/layouts/footer.tsx
@@ -1,12 +1,25 @@
+import { Github, Linkedin, Instagram } from "lucide-react";
+
 const Footer = () => {
   return (
-    <footer className="w-full py-10 p-5 border-t border-border bg-background sm:px-16">
+    <footer className="bg-card w-full py-10 p-5 border-t border-border bg-background sm:px-16">
       <div className="flex flex-col gap-8">
         <div>
           <h4 className="text-lg font-bold tracking-tight">Abiasa</h4>
           <p className="text-sm text-muted-foreground mt-1">
             Thank you for visiting my website.
           </p>
+        </div>
+        <div className="flex gap-5">
+          <a href="">
+            <Github size={16} />
+          </a>
+          <a href="">
+            <Linkedin size={16} />
+          </a>
+          <a href="">
+            <Instagram size={16} />
+          </a>
         </div>
         <div className="text-xs text-muted-foreground">
           &copy; {new Date().getFullYear()} Abiasa. All rights reserved.
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,14 +10,10 @@ const IndexPage = () => {
   return (
     <>
       <Navbar />
-      <main className="p-5 mt-5 sm:px-16">
+      <main className="p-5 py-10 sm:px-16">
         <div className="md:flex md:justify-between md:items-center">
           <div>
-            <img
-              src="/foto_profil_formal.png"
-              alt=""
-              className="h-14 w-14 rounded-full"
-            />
+            <img src="/profile.png" alt="" className="h-14 w-14 rounded-full" />
             <h1 className="text-3xl font-semibold mt-5">
               Hi there! I'm Rehan Abhiassa
             </h1>
@@ -43,50 +39,52 @@ const IndexPage = () => {
           </div>
         </div>
       </main>
-      <div className="mt-16 md:flex md:justify-between">
-        <Section className="bg-card/10 sm:px-16" title="My Experience">
-          <div className="flex flex-col gap-5 mt-4">
-            <ItemCard
-              icon={<GraduationCap />}
-              title="Frontend Dev at Gamelab Indonesia"
-              date="Agustus 2025 - November 2025"
-            />
-            <ItemCard
-              icon={<GraduationCap />}
-              title="Student at SMK Negeri 1 Cipeundeuy"
-              date="Current"
-            />
-            <ItemCard
-              icon={<GraduationCap />}
-              title="Student at MTS Negeri 4 KBB"
-              date="Juli 2020 - Juli 2023"
-            />
-          </div>
-        </Section>
-        <Section
-          className="bg-card/10 mt-5 sm:px-16"
-          title="Rewards & Sertificates"
-        >
-          <div className="flex flex-col gap-5 mt-4">
-            <ItemCard
-              icon={<GraduationCap />}
-              title="Frontend Dev at Gamelab Indonesia"
-              date="Agustus 2025 - November 2025"
+      <div className="bg-secondary">
+        <div className="mt-16 md:flex md:justify-between">
+          <Section className="bg-card/10 sm:px-16" title="My Experience">
+            <div className="flex flex-col gap-5 mt-4">
+              <ItemCard
+                icon={<GraduationCap />}
+                title="Frontend Dev at Gamelab Indonesia"
+                date="Agustus 2025 - November 2025"
+              />
+              <ItemCard
+                icon={<GraduationCap />}
+                title="Student at SMK Negeri 1 Cipeundeuy"
+                date="Current"
+              />
+              <ItemCard
+                icon={<GraduationCap />}
+                title="Student at MTS Negeri 4 KBB"
+                date="Juli 2020 - Juli 2023"
+              />
+            </div>
+          </Section>
+          <Section
+            className="bg-card/10 mt-5 sm:px-16"
+            title="Rewards & Sertificates"
+          >
+            <div className="flex flex-col gap-5 mt-4">
+              <ItemCard
+                icon={<GraduationCap />}
+                title="Frontend Dev at Gamelab Indonesia"
+                date="Agustus 2025 - November 2025"
+              />
+            </div>
+          </Section>
+        </div>
+        <Section className="mt-16 sm:px-16" title="My Projects">
+          <div className="grid grid-cols-1 gap-5 mt-7 mb-16 sm:grid-cols-2 md:grid-cols-3">
+            <ProjectCard
+              image="/landingpage.png"
+              title="Llama Network Landing Page"
+              description="Landing page created with Astro JS."
+              previewLink="#"
+              githubLink="#"
             />
           </div>
         </Section>
       </div>
-      <Section className="mt-16 mb-16 sm:px-16" title="My Projects">
-        <div className="grid grid-cols-1 gap-5 mt-7 sm:grid-cols-2 md:grid-cols-3">
-          <ProjectCard
-            image="/landingpage.png"
-            title="Llama Network Landing Page"
-            description="Landing page created with Astro JS."
-            previewLink="#"
-            githubLink="#"
-          />
-        </div>
-      </Section>
       <Footer />
     </>
   );
